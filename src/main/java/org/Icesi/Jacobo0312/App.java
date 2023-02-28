c44b46828781ac614252d079d8f5c2eb759c8646
Jacobo Garcia <72984810+Jacobo0312@users.noreply.github.com>
2023-02-28 08:48:36 -0500

    Add example cache aside

--- src/main/java/org/Icesi/Jacobo0312/App.java
+++ src/main/java/org/Icesi/Jacobo0312/App.java
@@ -3,6 +3,8 @@ package org.Icesi.Jacobo0312;
 import redis.clients.jedis.DefaultJedisClientConfig;
 import redis.clients.jedis.Jedis;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -10,25 +12,74 @@ import java.util.Set;
  */
 public class App {
 
+    private static boolean useSsl = true;
+    private static String cacheHostname = System.getenv("REDISCACHEHOSTNAME");
+    private static String cachekey = System.getenv("REDISCACHEKEY");
+
+    // Connect to the Azure Cache for Redis over the TLS/SSL port using the key.
+    private static final Jedis jedis = new Jedis(
+            cacheHostname,
+            6380,
+            DefaultJedisClientConfig.builder().password(cachekey).ssl(useSsl).build());
+
+    private static final Map<String, String> database = new HashMap<String, String>(
+            Map.of(
+                    "Colombia", "Bogota",
+                    "Peru", "Lima",
+                    "Chile", "Santiago",
+                    "Argentina", "Buenos Aires",
+                    "Brasil", "Brasilia",
+                    "Venezuela", "Caracas",
+                    "Uruguay", "Montevideo",
+                    "Paraguay", "Asuncion",
+                    "Bolivia", "La Paz",
+                    "Ecuador", "Quito"
+                    )
+    );
+
     public static void main(String[] args) {
 
-        boolean useSsl = true;
-        String cacheHostname = System.getenv("REDISCACHEHOSTNAME");
-        String cachekey = System.getenv("REDISCACHEKEY");
 
-        // Connect to the Azure Cache for Redis over the TLS/SSL port using the key.
-        Jedis jedis = new Jedis(cacheHostname, 6380, DefaultJedisClientConfig.builder().password(cachekey).ssl(useSsl).build());
+        showDatabase();
 
-        // Get all keys in cache
-        Set<String> keys = jedis.keys("*");
+        showCache();
 
-        // Iterate over keys and print values
-        for (String key : keys) {
-            System.out.println("Key: " + key + ", Value: " + jedis.get(key));
-        }
+        getCapital("Colombia");
+
+
 
 
         jedis.close();
+    }
+
+    private static void showDatabase() {
+        System.out.println("Database:");
+        for (Map.Entry<String, String> entry : database.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+    }
+
+    private static void showCache() {
+        System.out.println("Cache:");
+        Set<String> keys = jedis.keys("*");
+        for (String key : keys) {
+            System.out.println("Key: " + key + ", Value: " + jedis.get(key));
+        }
+    }
+
+    private static void getCapital(String country) {
+
+        System.out.println("Looking for capital of " + country);
+
+        String capital = jedis.get(country);
+        if (capital == null) {
+            capital = database.get(country);
+            jedis.set(country, capital);
+            System.out.println("Missed cache, added to cache.");
+        }else{
+            System.out.println("Hit cache.");
+        }
+        System.out.println("Capital of " + country + " is " + capital);
     }
 
 }
